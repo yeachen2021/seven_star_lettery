d6014407f31108c08384b8a822bd11ff75b59ef6
gongy <gongyiqing2014@163.com>
2023-09-25 23:05:53 +0800

    搭建环境

---
+++ src/test/java/com/sevevstar/lettery/SevenStarLetteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevevstar.lettery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevenStarLetteryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
